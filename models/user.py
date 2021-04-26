bcfde12475f15dd0b2c40ba60b8217d4f968de67
kjk1325e <kaitlinjkelley@gmail.comm>
2021-04-26 11:35:07 -0500

    Added register_new_user to request_handler

--- models/user.py
+++ models/user.py
@@ -1,13 +1,15 @@
 import datetime
+
 class User():
-    def __init__(self, first_name, last_name, email, bio, active = True, profile_image_url = ""):
+    def __init__(self, first_name, last_name, user_name, email, bio, created_on, active = True, profile_image_url = ""):
         self.first_name = first_name
         self.last_name = last_name
-        self.user_name 
+        self.display_name
+        self.user_name = user_name 
         self.email = email
         self.__password
         self.bio = bio
-        self.created_on = datetime.date.today()
+        self.created_on = created_on
         self.active = active
         self.profile_image_url = profile_image_url
 
@@ -17,6 +19,7 @@ class User():
         self.__password = password
 
     @property
-    def user_name(self, first_name, last_name):
+    def display_name(self, first_name, last_name):
         print("User Name")
-        self.user_name = f'{first_name} {last_name}'
+        self.display_name = f'{first_name} {last_name}'
+    
--- request_handler.py
+++ request_handler.py
@@ -1,9 +1,6 @@
 import json
 from http.server import BaseHTTPRequestHandler, HTTPServer
-from animals import get_all_animals, get_single_animal, delete_animal, update_animal, get_animals_by_location, get_animals_by_status, create_animal
-from customers import get_all_customers, get_single_customer, create_customer, delete_customer, update_customer, get_customers_by_email
-from employees import get_all_employees, get_single_employee, get_employees_by_location
-from locations import get_all_locations, get_single_location, delete_location, update_location
+from users import register_new_user
 
 
 # Here's a class. It inherits from another class.
@@ -137,8 +134,8 @@ class HandleRequests(BaseHTTPRequestHandler):
         # Add a new animal to the list. Don't worry about
         # the orange squiggle, you'll define the create_animal
         # function next.
-        if resource == "animals":
-            new_creation = create_animal(post_body)
+        if resource == "users":
+            new_creation = register_new_user(post_body)
         # elif resource == "customers":
         #     new_creation = create_customer(post_body)
         # elif resource == "employees":
