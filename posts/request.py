1aca8fe47fc39d969eb229b9ddd98ba5829af8df
Nick Carver <nicholascarver2@outlook.com>
2021-04-26 11:12:10 -0500

    built the get posts by users function

--- posts/request.py
+++ posts/request.py
@@ -4,30 +4,32 @@ from models import Post
 
 def get_posts_by_user_id(user_id):
 
-    with sqlite3.connect("./kennel.db") as conn:
+    with sqlite3.connect("./rare.db") as conn:
         conn.row_factory = sqlite3.Row
         db_cursor = conn.cursor()
 
         # Write the SQL query to get the information you want
         db_cursor.execute("""
         SELECT
-            a.id,
-            a.name,
-            a.breed,
-            a.status,
-            a.location_id,
-            a.customer_id
-        FROM animal a
-        WHERE a.status = ?
-        """, (status, ))
+            p.id,
+            p.user_id,
+            p.category_id,
+            p.title,
+            p.publication_date,
+            p.image_url,
+            p.content,
+            p.approved
+        FROM posts p
+        WHERE p.status = ?
+        """, (user_id, ))
 
-        animals = []
+        posts = []
         dataset = db_cursor.fetchall()
 
         for row in dataset:
-            animal = Animal(row['id'], row['name'], row['breed'],
-                            row['status'], row['location_id'],
-                            row['customer_id'])
-            animals.append(animal.__dict__)
+            post = Post(row['id'], row['user_id'], row['category_id'],
+                            row['title'], row['publication_date'],
+                            row['image_url'], row['content'], row['approved'])
+            posts.append(post.__dict__)
 
-    return json.dumps(animals)
+    return json.dumps(posts)
