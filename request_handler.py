dfc3a84b494afcda9f61995bf8a83df80f75fcce
Nick Carver <nicholascarver2@outlook.com>
2021-04-26 12:06:47 -0500

    had to remove some import statements that did not exist anymore

--- request_handler.py
+++ request_handler.py
@@ -1,8 +1,5 @@
 import json
 from http.server import BaseHTTPRequestHandler, HTTPServer
-from animals import get_all_animals, get_single_animal, delete_animal, update_animal, get_animals_by_location, get_animals_by_status, create_animal
-from customers import get_all_customers, get_single_customer, create_customer, delete_customer, update_customer, get_customers_by_email
-from employees import get_all_employees, get_single_employee, get_employees_by_location
 from posts import get_posts_by_user_id
 
 
