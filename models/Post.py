8df61380783eacdd949bdb8c1d7d4631bf48a5bb
Nick Carver <nicholascarver2@outlook.com>
2021-04-26 14:32:29 -0500

    got the boolean to work correctly in the get calls

--- models/Post.py
+++ models/Post.py
@@ -3,7 +3,7 @@ class Post():
     # Class initializer. It has 5 custom parameters, with the
     # special `self` parameter that every method on a class
     # needs as the first parameter.
-    def __init__(self, id, user_id, category_id, title, publication_date, image_url = "", content = "", approved = 0):
+    def __init__(self, id, user_id, category_id, title, publication_date, image_url = "", content = "", approved = True):
         self.id = id
         self.userId = user_id
         self.categoryId = category_id
